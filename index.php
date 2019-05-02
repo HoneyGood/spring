ef7a237be02762fa6e3003aa542b5faf4c4471e2
honey <honeyiamgood@mail.ru>
2019-05-03 00:33:38 +0300

    upload/download files

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
@@ -9,8 +9,12 @@
     <script src="js/dat.gui.js"></script>
     <script src="js/three.js"></script>
     <script src="js/OrbitControls.js"></script>
-    <script src="js/stats.min.js" defer></script>
-    <script src="js/STLLoader.js"></script>
+    <script src="js/stats.min.js" defer></script> <!--statistic-->
+    <script src="js/STLLoader.js"></script> <!--load from user stl file-->
+    <script src="js/STLExporter.js"></script> <!--export scene to stl-->
+    <script src="js/FileSaver.js"></script> <!--download from server-->
+
+
 
     <script type="text/javascript" src="js/jquery-3.3.1.js"></script>
 
@@ -24,29 +28,42 @@
 
     <!--MAIN SCRIPT-->
     <script defer type="text/javascript" src="js/main.js"></script>
+    <script defer type="text/javascript" src="js/fileloader.js"></script>
+
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Korzhov/Kazunin project</a>
+<nav class="navbar navbar-expand-lg dark_nav bg-dark">
+    <span class="navbar-brand">Korzhov/Kazunin project</span>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="form-inline my-2 my-lg-0" action="" method="post" enctype="multipart/form-data">
-            <ul class="navbar-nav m-auto">
+        <!--enctype="multipart/form-data"-->
+        <form class="form-inline my-2 my-lg-0 m-auto" action="" method="post" enctype="multipart/form-data">
+            <ul class="navbar-nav">
                 <li class="nav-item">
-                    <input type="file" class="form-control-file mr-sm-2" name="filename">
+                    <!--                    <input type="file" class="form-control-file mr-sm-2" name="filename">-->
+                    <input type="file" name="filename" id="file" class="input-file">
+                    <label for="file" class="btn btn-outline-light btn-left">
+                        <i class="icon fa fa-check"></i>
+                        <span class="js-fileName">Choose a file</span>
+                    </label>
                 </li>
                 <li class="nav-item">
-                    <input type="submit" class="btn btn-primary my-2 my-sm-0" value="Загрузить">
+                    <input id="load" type="submit" class="btn btn-outline-light my-sm-0 btn-right" value="Load">
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-2">
+                <li class="nav-item">
+                    <button class="btn btn-outline-light my-sm-0">Download</button>
                 </li>
             </ul>
         </form>
+
     </div>
 </nav>
-<div class="ajax-respond"></div>
 <div class="text-center" id="threejs">
 </div>
 
